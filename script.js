18c159d0b0032be53307d4cb904a0355c6b4acca
phischa <ph.git@mailbox.org>
2024-02-21 18:05:39 +0100

    first JSON

---
+++ script.js
@@ -0,0 +1,17 @@
+let questions = [
+    {   "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Taylor Swift",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Billie Joe Armstrong",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer hat JSON erfunden?",
+        "answer_1": "Steve Jobs",
+        "answer_2": "Douglas Crockford",
+        "answer_3": "Larry Page",
+        "answer_4": "Bill Gates",
+        "right_answer": 2
+    }
+];
