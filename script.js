bb19b9a5a84f80e9120f410c84ee9250cd24990b
phischa <ph.git@mailbox.org>
2024-02-23 00:01:12 +0100

    second script

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ let questions = [
 
 let currentQuestion = 0;
 
-function init(){
+function init() {
     document.getElementById("all-questions").innerHTML = questions.length;
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     let question = questions[currentQuestion];
 
     document.getElementById("question-text").innerHTML = question['question'];
@@ -32,4 +32,17 @@ function showQuestion(){
     document.getElementById("answer_2").innerHTML = question['answer_2'];
     document.getElementById("answer_3").innerHTML = question['answer_3'];
     document.getElementById("answer_4").innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    console.log('Selected ans is', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is', selectedQuestionNumber);
+    console.log('Current Right', question['right_answer']);
+    if (selectedQuestionNumber == question['right_answer']) {
+        console.log('Richtig')
+    } else {
+        console.log('FALSCH')
+    }
 }
