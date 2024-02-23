3c71f681f045194a390b0a85b44ce743290af339
phischa <ph.git@mailbox.org>
2024-02-23 14:52:48 +0100

    question logic

--- script.js
+++ script.js
@@ -17,10 +17,9 @@ let questions = [
     }
 ];
 
-let currentQuestion = 0;
-
+let currentQuestion = 0; //legt fest, dass die 1.Frage Stelle 0 ist
 function init() {
-    document.getElementById("all-questions").innerHTML = questions.length;
+    document.getElementById("all-questions").innerHTML = questions.length; //Zahl an der Stelle so lang wie die Zahl aller Fragen
     showQuestion();
 }
 
@@ -35,14 +34,35 @@ function showQuestion() {
 }
 
 function answer(selection) {
-    let question = questions[currentQuestion];
-    console.log('Selected ans is', selection);
-    let selectedQuestionNumber = selection.slice(-1);
-    console.log('selectedQuestionNumber is', selectedQuestionNumber);
-    console.log('Current Right', question['right_answer']);
-    if (selectedQuestionNumber == question['right_answer']) {
-        console.log('Richtig')
-    } else {
-        console.log('FALSCH')
+    let question = questions[currentQuestion]; //question = questions[0]
+    //console.log('Selected ans is', selection);
+    let selectedQuestionNumber = selection.slice(-1); //der letzte character von selection(als die jeweilige Zahl) wird extrhiert und ist gleich selectedQuestionNumber
+    /* console.log('selectedQuestionNumber is', selectedQuestionNumber);
+    console.log('Current Right', question['right_answer']); */
+    let idOfRightAnswer = `answer_${question['right_answer']}`; //${...} fügt die Zahl questions[right_answer][x] ein.
+    if (selectedQuestionNumber == question['right_answer']) { //Wenn Überieinstimmung
+        document.getElementById(selection).parentNode.classList.add('bg-success'); //parentNode um an die übergeordnete Div der id="answer_3" zu kommen und diese die Klasse hinzuzufügen.
+    } else { // Wenn keine Übereinstimmung
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+    document.getElementById("next-button").disabled = false; //Nächste Frage Button wird klickbar
+}
+
+function nextQuestion() {
+    currentQuestion++; //Variable wird z.B. von 0 auf 1 erhöht um auf die nächste Frage zuzugreifen
+    resetAddedClasses(); //Bevor die nächste Frage angezeigt wird, werden erstmal die added Classes entfernt
+    showQuestion(); //zeigt nächste Frage
+}
+
+function resetAddedClasses() {
+    document.getElementById("next-button").disabled = true;
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
 }
